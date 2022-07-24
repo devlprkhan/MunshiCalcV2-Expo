4495aa27158cfb361068fdcbaf2e1c79c53085b4
devlprkhan <devlpr.khan@gmail.com>
2022-07-24 18:04:53 +0500

    Last Update 24/7/22

--- src/Screens/Main/index.js
+++ src/Screens/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { Text, View, TextInput, FlatList, Pressable } from "react-native";
 import styles from "./styles";
 import { LinearGradient } from "expo-linear-gradient";
@@ -7,6 +7,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import Heading from "../../Components/Heading";
 // Data
 import data from "../../../assets/data/index.json";
+import { enableExpoCliLogging } from "expo/build/logs/Logs";
 // Extract (Coins, Notes, Bonds) From Data (for FlatList)
 let coins = data[0];
 let notes = data[1];
@@ -28,93 +29,144 @@ const initialValuesForNotes = {
 
 export default function Main() {
   // states
-  let [coinsData, setCoinsData] = useState(initialValuesForCoins)
-  let [notesData, setNotesData] = useState(initialValuesForNotes)
+  let [coinsData, setCoinsData] = useState(initialValuesForCoins);
+  let [notesData, setNotesData] = useState(initialValuesForNotes);
 
   // Multiply results
-  // This is for Coins input bar's
-  const [v5] = useState(() => value => 5 * value);
-  const [v2] = useState(() => value => 2 * value);
-  const [v1] = useState(() => value => 1 * value);
+  // This is for Coins input bar's (inline results)
+  const [v5] = useState(() => (value) => 5 * value);
+  const [v2] = useState(() => (value) => 2 * value);
+  const [v1] = useState(() => (value) => 1 * value);
 
-  // This is for Notes input bar's
-  const [v5000] = useState(() => value => 5000 * value);
-  const [v1000] = useState(() => value => 1000 * value);
-  const [v500] = useState(() => value => 500 * value);
-  const [v100] = useState(() => value => 100 * value);
-  const [v50] = useState(() => value => 50 * value);
-  const [v20] = useState(() => value => 20 * value);
-  const [v10] = useState(() => value => 10 * value);
-
+  // This is for Notes input bar's (inline results)
+  const [v5000] = useState(() => (value) => 5000 * value);
+  const [v1000] = useState(() => (value) => 1000 * value);
+  const [v500] = useState(() => (value) => 500 * value);
+  const [v100] = useState(() => (value) => 100 * value);
+  const [v50] = useState(() => (value) => 50 * value);
+  const [v20] = useState(() => (value) => 20 * value);
+  const [v10] = useState(() => (value) => 10 * value);
 
   // Update states with input data coming from input bars
   let setStates = (index, text, states) => {
     if (states === coinsData) {
       setCoinsData({
         ...coinsData,
-        [index]: text
-      })
+        [index]: text,
+      });
     } else {
       setNotesData({
         ...notesData,
-        [index]: text
-      })
+        [index]: text,
+      });
     }
-  }
+  };
 
-  // Multiplier Function
+  // Multiplier Function to multiply input values
   let multiplier = (value, pow) => {
-    return value * pow
-  }
+    return value * pow;
+  };
 
   // Function add comma in price
   let NumberWithCommas = (value) => {
     // regex to add comma in value
     return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+  };
 
   // Clear Inputs
   // Used to clear inputs and reset the states
   let clearInputs = () => {
     setCoinsData({ ...initialValuesForCoins });
     setNotesData({ ...initialValuesForNotes });
-  }
+  };
 
   // Get totatl
   // This function introduces sums of coins, notes and also total number of coins and notes
   let getTotal = () => {
     // Number of Coins
-    let totalNumOfCoins = Number(coinsData[0]) + Number(coinsData[1]) + Number(coinsData[2])
-    // Total Value
-    let totalCoin = v5(Number(coinsData[0])) + v2(Number(coinsData[1])) + v1(Number(coinsData[2]))
+    let totalNumOfCoins =
+      Number(coinsData[0]) + Number(coinsData[1]) + Number(coinsData[2]);
+    // Total Value of Coins
+    let totalCoin =
+      v5(Number(coinsData[0])) +
+      v2(Number(coinsData[1])) +
+      v1(Number(coinsData[2]));
 
     // Number of Notes
-    const totalNumOfNotes = Number(notesData[0]) + Number(notesData[1]) + Number(notesData[2]) + Number(notesData[3]) + Number(notesData[4]) + Number(notesData[5]) + Number(notesData[6])
-    // Total Value
-    const totalNote = v5000(Number(notesData[0])) + v1000(Number(notesData[1])) + v500(Number(notesData[2])) + v100(Number(notesData[3])) + v50(Number(notesData[4])) + v20(Number(notesData[5])) + v10(Number(notesData[6]));
+    const totalNumOfNotes =
+      Number(notesData[0]) +
+      Number(notesData[1]) +
+      Number(notesData[2]) +
+      Number(notesData[3]) +
+      Number(notesData[4]) +
+      Number(notesData[5]) +
+      Number(notesData[6]);
+    // Total Value of Coins
+    const totalNote =
+      v5000(Number(notesData[0])) +
+      v1000(Number(notesData[1])) +
+      v500(Number(notesData[2])) +
+      v100(Number(notesData[3])) +
+      v50(Number(notesData[4])) +
+      v20(Number(notesData[5])) +
+      v10(Number(notesData[6]));
 
     // Return as An Array
-    return [totalNumOfCoins, totalCoin, totalNumOfNotes, totalNote]
+    return [totalNumOfCoins, totalCoin, totalNumOfNotes, totalNote];
+  };
+
+  // test
+  var num = "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19".split(" ");
+  
+  function number2words(n){
+      if (n < 100) {
+        return n
+      }
+      else if (n < 1000) {
+        return (n%100 == 0? "": " " + number2words(n%100)) + "سو" + num[Math.floor(n/100)]
+      }
+      else if (n < 100000) {
+        return (n%1000 != 0? " " + number2words(n%1000): "") + "ہزار" + number2words(Math.floor(n/1000));
+      }
+      else if (n < 10000000) {
+        return (n%100000 != 0? " " + number2words(n%100000): "") + "لاکھ" + number2words(Math.floor(n/100000));
+      }
+      else if (n < 1000000000) {
+        return (n%10000000 != 0? " " + number2words(n%10000000): "") + "کروڑ" + number2words(Math.floor(n/10000000));
+      }
+      else if (n < 100000000000) {
+        return (n%1000000000 != 0? " " + number2words(n%1000000000): "") + "ارب" + number2words(Math.floor(n/1000000000));
+      }
+      else if (n < 10000000000000) {
+        return (n%100000000000 != 0? " " + number2words(n%100000000000): "") + "کھرب" + number2words(Math.floor(n/100000000000));
+      }
   }
 
-  // Access Function Return Values (Return As an Array)
+
+  console.log(number2words(8078953756800));
+  // console.log(number2words(8078953756830)); //Last two word main error hain jaisay 100 rupay tak thik 100 say nichay k word main error hain
+  // console.log(number2words(10000));
+  // test end
+
+  // Access Function Returned Values (Return As an Array)
   let [totalNumOfCoins, totalCoin, totalNumOfNotes, totalNote] = getTotal();
-  // Super Total
-  let superTotal = Number(totalCoin) + Number(totalNote)
+  // Super Total For Bottom Bar
+  // Get (totalCoins & totalNotes) from function named as (getTotal)
+  let superTotal = Number(totalCoin) + Number(totalNote);
 
   // FlatList Functions
   // Header
   const FlatList_Header = (props) => {
     return (
       <View>
-        <Text style={styles.notesHeading}>{props}</Text>
+        <Text style={styles.flatlistHeader}>{props}</Text>
       </View>
     );
   };
   // Footer
   const FlatList_Footer = (totalNumOf, total) => {
     return (
-      <View style={styles.totalContainer}>
+      <View style={styles.flatlistBottom}>
         <Text style={styles.rs}>روپئے {NumberWithCommas(total)}</Text>
         <View style={styles.line}></View>
         <Text style={styles.rs}>تعداد {NumberWithCommas(totalNumOf)}</Text>
@@ -138,9 +190,9 @@ export default function Main() {
           value={inputStates[index]}
           onChangeText={(text) => {
             if (inputStates === coinsData) {
-              setStates(index, text, coinsData)
+              setStates(index, text, coinsData);
             } else {
-              setStates(index, text, notesData)
+              setStates(index, text, notesData);
             }
           }}
           style={styles.input}
@@ -151,9 +203,15 @@ export default function Main() {
           colors={["#80c341", "#1eb24b"]}
           start={{ x: 1, y: 1 }}
           end={{ x: 0, y: 0 }}
-          style={styles.totalBg}
+          style={styles.inlineTotalBG}
         >
-          <Text style={[styles.price, {fontSize: 17}]}>{multiplier(item.val, Number(inputStates[index])) == 0 ? '0.00' : NumberWithCommas(multiplier(item.val, Number(inputStates[index])))}</Text>
+          <Text style={[styles.price, { fontSize: 17 }]}>
+            {multiplier(item.val, Number(inputStates[index])) == 0
+              ? "0.00"
+              : NumberWithCommas(
+                  multiplier(item.val, Number(inputStates[index]))
+                )}
+          </Text>
         </LinearGradient>
       </View>
     );
@@ -161,30 +219,41 @@ export default function Main() {
 
   return (
     <View style={styles.container}>
-      <View style={{flex: 1}}>
-        <Heading />
-        <View style={styles.notesContainer}>
-          {/* Flatlist to populate InputbBars for Notes */}
-          <FlatList
-            data={notes.notes}
-            renderItem={(item) => renderItemFunction(item, notesData)}
-            keyExtractor={(item) => item.id}
-            ListHeaderComponent={FlatList_Header(`BANK NOTES`)}
-            ListFooterComponent={() => FlatList_Footer(totalNumOfNotes, totalNote)}
-          />
-          {/* Flatlist to populate InputbBars for Coins */}
-          <FlatList
-            data={coins.coins}
-            renderItem={(item) => renderItemFunction(item, coinsData)}
-            keyExtractor={(item) => item.id}
-            ListHeaderComponent={FlatList_Header(`COIN'S`)}
-            ListFooterComponent={() => FlatList_Footer(totalNumOfCoins, totalCoin)}
-          />
-        </View>
+      {/* Heading */}
+      <Heading />
+      {/* Flatlist's Container */}
+      <View style={styles.flatlistsContainer}>
+        {/* Flatlist to populate InputbBars for Notes */}
+        <FlatList
+          data={notes.notes}
+          renderItem={(item) => renderItemFunction(item, notesData)}
+          keyExtractor={(item) => item.id}
+          ListHeaderComponent={FlatList_Header(`BANK NOTES`)}
+          ListFooterComponent={() =>
+            FlatList_Footer(totalNumOfNotes, totalNote)
+          }
+          // showsVerticalScrollIndicator={false}
+        />
+        {/* Flatlist to populate InputbBars for Coins */}
+        <FlatList
+          data={coins.coins}
+          renderItem={(item) => renderItemFunction(item, coinsData)}
+          keyExtractor={(item) => item.id}
+          ListHeaderComponent={FlatList_Header(`COIN'S`)}
+          ListFooterComponent={() =>
+            FlatList_Footer(totalNumOfCoins, totalCoin)
+          }
+          // showsVerticalScrollIndicator={false}
+        />
       </View>
 
+      {/* Bottom Bar */}
       <View style={styles.bottomBar}>
-        <Text style={styles.superTotal}>Total: {NumberWithCommas(superTotal)}</Text>
+        {/* Super Total */}
+        <Text style={styles.superTotal}>
+          Total: {NumberWithCommas(superTotal)}
+        </Text>
+        {/* Clear Button */}
         <Pressable onPress={clearInputs}>
           <LinearGradient
             colors={["#80c341", "#1eb24b"]}
--- src/Screens/Main/styles.js
+++ src/Screens/Main/styles.js
@@ -1,130 +1,141 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "space-between",
-          backgroundColor: "#232323",
 
-    },
-    notesContainer: {
-      // flex: 1,
-      marginTop: 20,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    notesHeading: {
-      color: "#fff",
-      letterSpacing: 5,
-      fontFamily: "RalewayRegular",
-      fontSize: 13,
-      textAlign: "center",
-    },
-    // Inputs
-    inputContainer: {
-      flex:1,
-      marginTop: 10,
-      flexDirection: "row",
-    },
-    input: {
-      width: 90.2,
-      padding: 5,
-      fontFamily: "RalewayRegular",
-      textAlign: "center",
-      fontSize: 18,
-      color: "#fff",
-      borderColor: "#1EB24B",
-      borderWidth: 1,
-    },
-    // Price
-    priceBg: {
-      height: 40,
-      width: 110,
-      alignItems: "center",
-      justifyContent: "center",
-      borderBottomLeftRadius: 5,
-      borderTopLeftRadius: 5,
-    },
-    price: {
-      fontFamily: "RalewayMedium",
-      fontSize: 20,
-      color: "#fff",
-    },
-    // Total inline
-    totalBg: {
-      height: 40,
-      width: 110,
-      alignItems: "center",
-      justifyContent: "center",
-      borderBottomRightRadius: 5,
-      borderTopRightRadius: 5,
-    },
-    
-    // Super Total Bottom Bar
-    superTotal: {
-      fontFamily: "RalewayMedium",
-      fontSize: 20,
-      fontStyle: 'italic'
-    },
-  
-    // Total
-    totalContainer: {
-      // flexDirection: 'row',
-      // justifyContent: 'space-between',
-      marginTop: 10,
-      marginBottom: 20,
-    },
-    rs: {
-      fontFamily: "RalewayMedium",
-      // fontStyle: "italic",
-      fontSize: 15,
-      letterSpacing: 2,
-      color: "#fff",
-    },
-    totalValue: {
-      color: "white",
-      fontFamily: "RalewayMedium",
-    },
-    line: {
-      borderBottomWidth: 1,
-      borderBottomColor: "#1EB24B",
-      width: 160,
-      marginVertical: 5,
-      alignSelf: 'flex-end'
-    },
+  // Main Container
+  container: {
+    flex: 1,
+    backgroundColor: "#232323",
+  },
 
-    // Bottom Bar
-    bottomBar: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: 'center',
-      backgroundColor: "#fff",
-      padding: 10,
-      borderTopLeftRadius: 10,
-      borderTopRightRadius: 10,
-      
-    },
-    clearBtn: {
-      marginRight: 5,
-      paddingHorizontal: 7,
-      paddingVertical: 5,
-      borderRadius: 5
-    },
-    btnText: {
-      fontFamily: "RalewayRegular",
-      color: '#fff'
-    },
-    // Shadow
-    shadowProp: {
-      shadowColor: '#171717',
-      shadowOffset: {width: -3, height: 4},
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-  
-      elevation: 20,
-      shadowColor: '#171717',
-    },
-  });
-  
+  // Flatlists Container
+  flatlistsContainer: {
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-export default styles
+  // Flatlist Header Components
+  flatlistHeader: {
+    color: "#fff",
+    letterSpacing: 5,
+    fontFamily: "RalewayRegular",
+    fontSize: 13,
+    textAlign: "center",
+  },
+
+  // Flatlist Bottom Components
+  // Total
+  flatlistBottom: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  rs: {
+    fontFamily: "RalewayMedium",
+    fontSize: 15,
+    letterSpacing: 2,
+    color: "#fff",
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#1EB24B",
+    width: 160,
+    marginVertical: 5,
+    alignSelf: "flex-end",
+  },
+
+  // RenderItem Function (Flatlist)
+  // Inputs Container
+  inputContainer: {
+    flex: 1,
+    marginTop: 10,
+    flexDirection: "row",
+  },
+
+  // Price BG
+  priceBg: {
+    height: 35,
+    width: 110,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
+  },
+  //Price Text
+  price: {
+    fontFamily: "RalewayMedium",
+    fontSize: 20,
+    color: "#fff",
+  },
+
+  // Input Bar
+  input: {
+    width: 90.2,
+    height: 35,
+    fontFamily: "RalewayRegular",
+    textAlign: "center",
+    fontSize: 18,
+    color: "#fff",
+    borderColor: "#1EB24B",
+    borderWidth: 1,
+  },
+
+  // Total BG Inline
+  inlineTotalBG: {
+    height: 35,
+    width: 110,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomRightRadius: 5,
+    borderTopRightRadius: 5,
+  },
+
+  // Bottom Bar Container
+  bottomBar: {
+    //Set position of bottom bar based on keyboard popup 
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    // Top Curves
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  // Clear Button Container
+  clearBtn: {
+    marginRight: 5,
+    paddingHorizontal: 7,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  // Clear Button Text
+  btnText: {
+    fontFamily: "RalewayRegular",
+    color: "#fff",
+  },
+
+  // Super Total Bottom Bar
+  superTotal: {
+    fontFamily: "RalewayMedium",
+    fontSize: 20,
+    fontStyle: "italic",
+  },
+
+  
+  // Shadows
+  // Todo
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -3, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 20,
+    shadowColor: "#171717",
+  },
+});
+
+export default styles;
